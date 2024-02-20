4a011a10ce767d62eaf1ead21b98ba78e26f9b49
Simon Uhl <uhl.simon@gmail.com>
2024-02-20 11:02:10 +0100

    updating frontend --> still wip

--- ANTicle/forms.py
+++ ANTicle/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 class InputDataForm(forms.Form):
-    Quelle_1 = forms.CharField(max_length=100)
-    Quelle_2 = forms.CharField(max_length=100)
-    Quelle_3 = forms.CharField(max_length=100)
-    Quelle_4 = forms.CharField(max_length=100)
-    Quelle_5 = forms.CharField(max_length=100)
+    Quelle_1 = forms.CharField(required=True, max_length=10000)
+    Quelle_2 = forms.CharField(required=True, max_length=10000)
+    Quelle_3 = forms.CharField(required=False, max_length=10000)
+    Quelle_4 = forms.CharField(required=False, max_length=10000)
+    Quelle_5 = forms.CharField(required=False, max_length=10000)
