ad3e17a37a82ff478666a55610a48b2a94d610e1
Simon Uhl <uhl.simon@gmail.com>
2024-03-04 14:09:08 +0100

    removed requirement for last field V0.7.3.4 --> open for publik test

--- ANTicle/forms.py
+++ ANTicle/forms.py
@@ -6,5 +6,5 @@ class InputDataForm(forms.Form):
     Quelle_4 = forms.CharField(required=False, max_length=100000, widget=forms.Textarea(attrs={'class' : 'align-top'}))
     Quelle_5 = forms.CharField(required=False, max_length=100000, widget=forms.Textarea(attrs={'class' : 'align-top'}))
     words = forms.IntegerField(required=True, max_value=5000, widget=forms.NumberInput)
-    thema = forms.CharField(required=True, max_length=1000, widget=forms.Textarea(attrs={'class': 'align-top'}))
+    thema = forms.CharField(required=False, max_length=1000, widget=forms.Textarea(attrs={'class': 'align-top'}))
 
